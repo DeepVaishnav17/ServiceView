c7cf0024d48cdce737601cdf12dff155f8b5db1e
DeepVaishnav17 <deepvaishnav207@gmail.com>
2026-06-08 11:26:27 +0530

    Dockerized complete architecture now each service running inside containers

--- order-service/src/main/java/com/techieprogramme/order_service/service/OrderService.java
+++ order-service/src/main/java/com/techieprogramme/order_service/service/OrderService.java
@@ -51,7 +51,7 @@ public class OrderService {
                 .toList();
 
         InventoryResponse[] inventoryResponses = webClient.get()
-                .uri("http://localhost:8082/api/inventory",
+                .uri("http://inventory-service:8082/api/inventory",
                         uriBuilder -> uriBuilder.queryParam("skuCode", skuCodes).build())
                 .retrieve()
                 .bodyToMono(InventoryResponse[].class)
