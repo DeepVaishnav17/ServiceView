2905e4aef235f9ba7dc5150c3b6729f76450b1a6
DeepVaishnav17 <deepvaishnav207@gmail.com>
2026-06-12 23:15:48 +0530

    added kafka to locally run

--- order-service/src/main/java/com/techieprogramme/order_service/service/OrderService.java
+++ order-service/src/main/java/com/techieprogramme/order_service/service/OrderService.java
@@ -69,17 +69,7 @@ public class OrderService {
 
         if (Boolean.TRUE.equals(isSuccess)) {
             orderRepository.save(order);
-            // kafkaTemplate.send("notificationTopic", new OrderPlacedEvent(order.getOrderNumber()));
-            try {
-                webClientBuilder.build().post()
-                        .uri("http://notification-service/api/notification")
-                        .bodyValue(new OrderPlacedEvent(order.getOrderNumber()))
-                        .retrieve()
-                        .bodyToMono(Void.class)
-                        .block();
-            } catch (Exception e) {
-                System.err.println("Failed to send notification via REST: " + e.getMessage());
-            }
+            kafkaTemplate.send("notificationTopic", new OrderPlacedEvent(order.getOrderNumber()));
             return "Order Placed Successfully";
         } else {
             throw new IllegalArgumentException("Product is not in stock, please try again later");
